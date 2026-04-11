c61f1e8f7332582f1a431e7518176178f11c224f
limpanJJ <linusbeckman1@gmail.com>
2026-04-11 14:32:36 +0200

    Add WorkoutSessionController scaffold

---
+++ WorkoutTrackerAPI/Controllers/WorkoutSessionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WorkoutSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
