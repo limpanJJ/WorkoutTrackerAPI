1fbd95eedd19d5cf2d31c1c7dce1261659d0c73f
limpanJJ <linusbeckman1@gmail.com>
2026-04-20 14:03:31 +0200

    update data annotation

--- WorkoutTrackerAPI/Dtos/Exercises/Categories/Requests/UpdateCategoryRequest.cs
+++ WorkoutTrackerAPI/Dtos/Exercises/Categories/Requests/UpdateCategoryRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace WorkoutTrackerAPI.Dtos.Exercises.Categories.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutTrackerAPI.Dtos.Exercises.Categories.Requests
 {
     public class UpdateCategoryRequest
     {
+        [Required]
         public string Name { get; set; } = string.Empty;
     }
 }
